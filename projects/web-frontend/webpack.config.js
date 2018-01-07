c2562f7a9027e988ece17f9e945c62308f18c51f
Matthew Riley <matthew@matthewriley.name>
2018-01-07 22:39:47 +1100

    refactor to serverless

---
+++ projects/web-frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: ['./app/main.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                API_URL: JSON.stringify(process.env.API_URL),
+                GOOGLE_RECAPTCHA_SITE_KEY: JSON.stringify(process.env.GOOGLE_RECAPTCHA_SITE_KEY),
+                GOOGLE_ANALYTICS_TRACKING_ID: JSON.stringify(process.env.GOOGLE_ANALYTICS_TRACKING_ID)
+            }
+        })
+    ]
+};
---
+++ setenv.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export STAGE=${1:-dev}
+export AWS_PROFILE="charliemoukbel-deploy"
+export AWS_SES_REGION="us-west-2"
+export AWS_SES_IDENTITY="charliemoukbel.com"
+export NOTIFICATION_FROM_ADDRESS="charliemoukbel.com <noreply@charliemoukbel.com>"
+export GOOGLE_RECAPTCHA_SITE_KEY="6LeQMAsUAAAAAAm_KMxWOIpbdt3Lzung3U0Jaseh"
+export GOOGLE_ANALYTICS_TRACKING_ID="UA-86847178-1"
+export HOSTED_ZONE="charliemoukbel.com."
+export MESSAGES_TABLE_NAME="messages-$STAGE"
+
+if [ $STAGE = "dev" ]
+then
+    export STAGE="dev"
+    export NOTIFICATION_TO_ADDRESS="matthewriley.charliemoukbel@gmail.com"
+    export DOMAIN_NAME="dev.charliemoukbel.com"
+    export API_URL="https://75bsmtwvu3.execute-api.ap-southeast-2.amazonaws.com/dev"
+fi
+
+if [ $STAGE = "prod" ]
+then
+    export STAGE="prod"
+    export NOTIFICATION_TO_ADDRESS="cmoukbel@hotmail.com"
+    export DOMAIN_NAME="charliemoukbel.com"
+    export API_URL=""
+fi
