5ca9ca831636440539d25f2a6954da2444c6bbcd
Matthew Riley <matthew@matthewriley.name>
2018-01-11 18:29:00 +1100

    fix incorrect use of lodash omit

--- projects/web-backend/lib/save-message.js
+++ projects/web-backend/lib/save-message.js
@@ -7,7 +7,7 @@ module.exports = function (message) {
 
     const id = uuid();
     const createdOn = new Date(now()).toISOString();
-    const item = _.chain(message).pick(recordAttrs).omit(v => !v).assign({ id, createdOn }).value();
+    const item = _.chain(message).pick(recordAttrs).omitBy(v => !v).assign({ id, createdOn }).value();
     const params = { TableName: tableName, Item: item };
     return Promise.promisify(dynamoClient.put)(params);
 };
