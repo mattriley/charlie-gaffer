f4bdc4a49c798fdc1d37c7b187605fbe4c604346
Matthew Riley <matthew@matthewriley.name>
2018-03-30 12:33:13 +1100

    add eslint

--- projects/web-frontend/lib/data.js
+++ projects/web-frontend/lib/data.js
@@ -1,181 +1,181 @@
 module.exports = {
-    "portfolio": {
-        "collections": [
+    'portfolio': {
+        'collections': [
             {
-                "title": "Short Film",
-                "projects": [
+                'title': 'Short Film',
+                'projects': [
                     {
-                        "title": "Where We Live",
-                        "director": "Jesse Richards",
-                        "cinematographer": "Max Walter",
-                        "images": [
-                            "where-we-live-1.jpg",
-                            "where-we-live-2.jpg",
-                            "where-we-live-3.jpg",
-                            "where-we-live-4.jpg"
+                        'title': 'Where We Live',
+                        'director': 'Jesse Richards',
+                        'cinematographer': 'Max Walter',
+                        'images': [
+                            'where-we-live-1.jpg',
+                            'where-we-live-2.jpg',
+                            'where-we-live-3.jpg',
+                            'where-we-live-4.jpg'
                         ]
                     },
                     {
-                        "title": "Heart Station",
-                        "director": "Tao Jia",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "heart-station-1.jpg",
-                            "heart-station-2.jpg",
-                            "heart-station-3.jpg",
-                            "heart-station-4.jpg"
+                        'title': 'Heart Station',
+                        'director': 'Tao Jia',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'heart-station-1.jpg',
+                            'heart-station-2.jpg',
+                            'heart-station-3.jpg',
+                            'heart-station-4.jpg'
                         ]
                     },
                     {
-                        "title": "Kawaki and Sharp",
-                        "director": "Sam Steele",
-                        "cinematographer": "Max Walter",
-                        "images": [
-                            "kawaki-1.jpg",
-                            "kawaki-2.jpg"
+                        'title': 'Kawaki and Sharp',
+                        'director': 'Sam Steele',
+                        'cinematographer': 'Max Walter',
+                        'images': [
+                            'kawaki-1.jpg',
+                            'kawaki-2.jpg'
                         ]
                     },
                     {
-                        "title": "Warm Blood: Music clip",
-                        "director": "Alex Wu",
-                        "cinematographer": "Max Walter",
-                        "images": [
-                            "warm-blood-1.jpg",
-                            "warm-blood-2.jpg",
-                            "warm-blood-3.jpg",
-                            "warm-blood-4.jpg"
+                        'title': 'Warm Blood: Music clip',
+                        'director': 'Alex Wu',
+                        'cinematographer': 'Max Walter',
+                        'images': [
+                            'warm-blood-1.jpg',
+                            'warm-blood-2.jpg',
+                            'warm-blood-3.jpg',
+                            'warm-blood-4.jpg'
                         ]
                     },
                     {
-                        "title": "Peach",
-                        "director": "Carnelian Rose",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "peach-1.jpg",
-                            "peach-2.jpg",
-                            "peach-3.jpg",
-                            "peach-4.jpg"
+                        'title': 'Peach',
+                        'director': 'Carnelian Rose',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'peach-1.jpg',
+                            'peach-2.jpg',
+                            'peach-3.jpg',
+                            'peach-4.jpg'
                         ]
                     },
                     {
-                        "title": "Waiting on Sound",
-                        "director": "Samuel Galloway",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "waiting-on-sound-1.jpg",
-                            "waiting-on-sound-2.jpg",
-                            "waiting-on-sound-3.jpg",
-                            "waiting-on-sound-4.jpg"
+                        'title': 'Waiting on Sound',
+                        'director': 'Samuel Galloway',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'waiting-on-sound-1.jpg',
+                            'waiting-on-sound-2.jpg',
+                            'waiting-on-sound-3.jpg',
+                            'waiting-on-sound-4.jpg'
                         ]
                     }
                 ]
             },
             {
-                "title": "Commercial",
-                "projects": [
+                'title': 'Commercial',
+                'projects': [
                     {
-                        "title": "MS Australia",
-                        "cinematographer": "Max Walter",
-                        "images": [
-                            "ms-1.jpg",
-                            "ms-2.jpg",
-                            "ms-3.jpg",
-                            "ms-4.jpg"
+                        'title': 'MS Australia',
+                        'cinematographer': 'Max Walter',
+                        'images': [
+                            'ms-1.jpg',
+                            'ms-2.jpg',
+                            'ms-3.jpg',
+                            'ms-4.jpg'
                         ]
                     },
                     {
-                        "title": "Victoria Racing Commission",
-                        "subtitle": "Racing Royalty",
-                        "cinematographer": "Matt Gdanitz",
-                        "images": [
-                            "racing-royalty-1.jpg",
-                            "racing-royalty-2.jpg",
-                            "racing-royalty-3.jpg",
-                            "racing-royalty-4.jpg"
+                        'title': 'Victoria Racing Commission',
+                        'subtitle': 'Racing Royalty',
+                        'cinematographer': 'Matt Gdanitz',
+                        'images': [
+                            'racing-royalty-1.jpg',
+                            'racing-royalty-2.jpg',
+                            'racing-royalty-3.jpg',
+                            'racing-royalty-4.jpg'
                         ]
                     },
                     {
-                        "title": "Brown Brothers",
-                        "company": "Prosecco Wines",
-                        "cinematographer": "Matt Gdanitz",
-                        "images": [
-                            "prosecco-1.jpg",
-                            "prosecco-2.jpg",
-                            "prosecco-3.jpg",
-                            "prosecco-4.jpg"
+                        'title': 'Brown Brothers',
+                        'company': 'Prosecco Wines',
+                        'cinematographer': 'Matt Gdanitz',
+                        'images': [
+                            'prosecco-1.jpg',
+                            'prosecco-2.jpg',
+                            'prosecco-3.jpg',
+                            'prosecco-4.jpg'
                         ]
                     },
                     {
-                        "title": "Teeth Magazine - Black Sheep Edition",
-                        "cinematographer": "Amy Dellar",
-                        "images": [
-                            "black-sheep-1.jpg",
-                            "black-sheep-2.jpg",
-                            "black-sheep-3.jpg",
-                            "black-sheep-4.jpg"
+                        'title': 'Teeth Magazine - Black Sheep Edition',
+                        'cinematographer': 'Amy Dellar',
+                        'images': [
+                            'black-sheep-1.jpg',
+                            'black-sheep-2.jpg',
+                            'black-sheep-3.jpg',
+                            'black-sheep-4.jpg'
                         ]
                     },
                     {
-                        "title": "Violet 1 AM Magazine",
-                        "cinematographer": "Amy Dellar",
-                        "images": [
-                            "violet-1.jpg",
-                            "violet-2.jpg",
-                            "violet-3.jpg",
-                            "violet-4.jpg"
+                        'title': 'Violet 1 AM Magazine',
+                        'cinematographer': 'Amy Dellar',
+                        'images': [
+                            'violet-1.jpg',
+                            'violet-2.jpg',
+                            'violet-3.jpg',
+                            'violet-4.jpg'
                         ]
                     },
                     {
-                        "title": "Fair Work Australia P.A.C.T",
-                        "subtitle": "Monster and Bear Production",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "fair-work-1.jpg",
-                            "fair-work-2.jpg",
-                            "fair-work-3.jpg",
-                            "fair-work-4.jpg"
+                        'title': 'Fair Work Australia P.A.C.T',
+                        'subtitle': 'Monster and Bear Production',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'fair-work-1.jpg',
+                            'fair-work-2.jpg',
+                            'fair-work-3.jpg',
+                            'fair-work-4.jpg'
                         ]
                     },
                     {
-                        "title": "Akuna Blue",
-                        "subtitle": "Monster and Bear Production",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "akuna-1.jpg",
-                            "akuna-2.jpg"
+                        'title': 'Akuna Blue',
+                        'subtitle': 'Monster and Bear Production',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'akuna-1.jpg',
+                            'akuna-2.jpg'
                         ]
                     },
                     {
-                        "title": "Bertocchi",
-                        "subtitle": "Monster and Bear Production",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "bertocchi-1.jpg",
-                            "bertocchi-2.jpg",
-                            "bertocchi-3.jpg",
-                            "bertocchi-4.jpg"
+                        'title': 'Bertocchi',
+                        'subtitle': 'Monster and Bear Production',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'bertocchi-1.jpg',
+                            'bertocchi-2.jpg',
+                            'bertocchi-3.jpg',
+                            'bertocchi-4.jpg'
                         ]
                     },
                     {
-                        "title": "Telstra  -  One Drive",
-                        "subtitle": "Monster and Bear Production",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "telstra-one-drive-1.jpg",
-                            "telstra-one-drive-2.jpg",
-                            "telstra-one-drive-3.jpg",
-                            "telstra-one-drive-4.jpg"
+                        'title': 'Telstra  -  One Drive',
+                        'subtitle': 'Monster and Bear Production',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'telstra-one-drive-1.jpg',
+                            'telstra-one-drive-2.jpg',
+                            'telstra-one-drive-3.jpg',
+                            'telstra-one-drive-4.jpg'
                         ]
                     },
                     {
-                        "title": "Telstra",
-                        "subtitle": "Monster and Bear Production",
-                        "cinematographer": "Josh Mitchell-Frey",
-                        "images": [
-                            "telstra-1.jpg",
-                            "telstra-2.jpg",
-                            "telstra-3.jpg",
-                            "telstra-4.jpg"
+                        'title': 'Telstra',
+                        'subtitle': 'Monster and Bear Production',
+                        'cinematographer': 'Josh Mitchell-Frey',
+                        'images': [
+                            'telstra-1.jpg',
+                            'telstra-2.jpg',
+                            'telstra-3.jpg',
+                            'telstra-4.jpg'
                         ]
                     }
                 ]
