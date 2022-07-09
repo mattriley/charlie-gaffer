fb72eda068dc9ae2d69be27641f5901719c5101a
Matt Riley <mattrileyaus@gmail.com>
2022-07-09 15:47:09 +1000

    Unspecified changes

--- web-backend/src/modules/io/setup.js
+++ web-backend/src/modules/io/setup.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
-const uuid = require('node-uuid').v4;
 const fetch = require('node-fetch');
+const crypto = require('crypto');
 
 module.exports = ({ config }) => () => {
 
     return {
         fetch,
-        uuid,
+        uuid: crypto.randomUUID,
         now: Date.now,
         dynamoClient: new AWS.DynamoDB.DocumentClient(),
         sesClient: new AWS.SES({ region: config.awsSesRegion }),
