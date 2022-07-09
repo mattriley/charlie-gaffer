4d47688f836f8be9e92ae827791c3356de9b4985
Matt Riley <mattrileyaus@gmail.com>
2022-07-09 20:28:08 +1000

    Unspecified changes

--- backend/src/lambda.js
+++ backend/src/lambda.js
@@ -1,6 +1,6 @@
 const compose = require('./compose');
 
-module.exports = (event, context, callback) => {
+module.exports.handler = async (event, context, callback) => {
 
     const { modules } = compose();
     return modules.contactMe.lambda(event, context, callback);
