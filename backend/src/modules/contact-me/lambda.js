b4fe9361b537ce10d83ee42158002c26966c4dec
Matt Riley <mattrileyaus@gmail.com>
2022-07-09 21:05:37 +1000

    Unspecified changes

--- backend/src/modules/contact-me/lambda.js
+++ backend/src/modules/contact-me/lambda.js
@@ -1,6 +1,6 @@
 module.exports = ({ contactMe }) => async event => {
 
-    const message = event.body;
+    const message = JSON.parse(event.body);
     await contactMe.verifyCaptcha(message.grecaptchaResponse);
     await contactMe.saveMessage(message);
     await contactMe.sendEmail(message);
