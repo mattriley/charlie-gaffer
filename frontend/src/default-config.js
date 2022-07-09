7eca1d1e6c6ad58aa98a1da3b723c760c4f79ba8
Matt Riley <mattrileyaus@gmail.com>
2022-07-09 19:35:10 +1000

    Unspecified changes

--- frontend/src/default-config.js
+++ frontend/src/default-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    app: {
+        name: 'Charlie Moukbel - Gaffer'
+    },
     portfolio: require('./portfolio'),
     apiUrl: 'https://api.charliemoukbel.com/prod',
     googleRecaptchaSiteKey: '6LeQMAsUAAAAAAm_KMxWOIpbdt3Lzung3U0Jaseh',
