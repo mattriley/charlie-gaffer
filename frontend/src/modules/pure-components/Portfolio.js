ae298cfad4c5c23b79be06feee78018bfc824055
Matt Riley <mattrileyaus@gmail.com>
2022-07-10 16:51:51 +1000

    Unspecified changes

--- frontend/src/modules/pure-components/Portfolio.js
+++ frontend/src/modules/pure-components/Portfolio.js
@@ -1,54 +1,49 @@
-const React = require('react');
+module.exports = ({ config }) => () => {
 
-module.exports = ({ config }) => {
-
-    return class Portfolio extends React.Component {
-        render() {
-            var collections = config.portfolio.collections.map((collection, i) => this.renderCollection(collection, i));
-            return <div className="portfolio">{collections}</div>;
-        }
-
-        renderCollection(collection, i) {
-            var projects = collection.projects.map((project, i) => this.renderProject(project, i));
-            return <div key={i} className="collection">
-                <h1>{collection.title}</h1>
-                {projects}
-            </div>;
-        }
-
-        renderProject(project, i) {
-            var images = project.images.map((image, i) => this.renderImage(image, i));
-
-            var filmInfo = [];
-
-            if (project.subtitle) {
-                filmInfo.push(project.subtitle);
-            }
-
-            if (project.director) {
-                filmInfo.push('Director: ' + project.director);
-            }
-
-            if (project.cinematographer) {
-                filmInfo.push('Cinematographer: ' + project.cinematographer);
-            }
-
-            filmInfo.push('Gaffer: Charlie Moukbel');
-
-            var fileInfoElements = filmInfo.map((item, i) => <div key={i}>{item}</div>);
-
-            return <div key={i} className="project">
-                <h2>{project.title}</h2>
-                {images}
-                <div className="project-details">
-                    {fileInfoElements}
-                </div>
-            </div>;
-        }
-
-        renderImage(image, i) {
-            var src = 'screenshots/' + image;
-            return <img key={i} src={src} />;
-        }
+    const renderCollection = (collection, i) => {
+        const projects = collection.projects.map(renderProject);
+        return <div key={i} className="collection">
+            <h1>{collection.title}</h1>
+            {projects}
+        </div>;
     };
+
+    const renderProject = (project, i) => {
+        const images = project.images.map(renderImage);
+
+        const filmInfo = [];
+
+        if (project.subtitle) {
+            filmInfo.push(project.subtitle);
+        }
+
+        if (project.director) {
+            filmInfo.push('Director: ' + project.director);
+        }
+
+        if (project.cinematographer) {
+            filmInfo.push('Cinematographer: ' + project.cinematographer);
+        }
+
+        filmInfo.push('Gaffer: Charlie Moukbel');
+
+        const fileInfoElements = filmInfo.map((item, i) => <div key={i}>{item}</div>);
+
+        return <div key={i} className="project">
+            <h2>{project.title}</h2>
+            {images}
+            <div className="project-details">
+                {fileInfoElements}
+            </div>
+        </div>;
+    };
+
+    const renderImage = (image, i) => {
+        const src = 'screenshots/' + image;
+        return <img key={i} src={src} />;
+    };
+
+    const collections = config.portfolio.collections.map(renderCollection);
+    return <div className="portfolio">{collections}</div>;
+
 };
