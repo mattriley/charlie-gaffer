7b003d0cf0be869a52cecd83e764ee73a2dae300
Matt Riley <mattrileyaus@gmail.com>
2022-07-10 17:47:44 +1000

    Unspecified changes

--- frontend/src/default-config.js
+++ frontend/src/default-config.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-undef */
+
 module.exports = {
-    app: {
-        name: 'Charlie Moukbel - Gaffer'
-    },
     portfolio: require('./portfolio'),
-    apiUrl: 'https://dvyxxovnfx7szz4ojnchxmww5q0fnvto.lambda-url.ap-southeast-2.on.aws/', //test
-    googleRecaptchaSiteKey: '6LeQMAsUAAAAAAm_KMxWOIpbdt3Lzung3U0Jaseh',
-    googleAnalyticsTrackingId: 'UA-86847178-1'
+    app: {
+        name: process.env.PROJECT_NAME
+    },
+    stage: process.env.STAGE,
+    apiUrl: process.env.CONTACT_URL,
+    googleRecaptchaSiteKey: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
+    googleAnalyticsTrackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID
 };
