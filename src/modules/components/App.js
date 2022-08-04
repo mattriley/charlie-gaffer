0a371387c0023626fd1ac83f618745f11bffc3e5
Matt Riley <mattrileyaus@gmail.com>
2022-08-04 22:28:23 +1000

    Unspecified changes

--- src/modules/components/App.js
+++ src/modules/components/App.js
@@ -1,6 +1,9 @@
 module.exports = ({ components, pureComponents }) => () => {
 
     return <div>
+        <div id="header">
+            <a href="#contact-me">CONTACT ME</a>
+        </div>
         <pureComponents.TitleBar />
         <pureComponents.Portfolio />
         <components.ContactMe />
--- src/modules/pure-components/TitleBar.js
+++ src/modules/pure-components/TitleBar.js
@@ -3,7 +3,7 @@ module.exports = () => () => {
     return <div className="title">
         <img src="images/steamrobo.png" />
         <div className="name">Charlie / Gaffer / Melbourne</div>
-        <a href="#contact-me">Get in touch</a>
+
     </div>;
 
 };
