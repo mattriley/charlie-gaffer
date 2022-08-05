6fa0e9aae13a88857720188c2a405b30f1bdb7d4
Matt Riley <mattrileyaus@gmail.com>
2022-08-05 10:02:54 +1000

    Unspecified changes

--- src/modules/components/App.js
+++ src/modules/components/App.js
@@ -1,11 +1,15 @@
 module.exports = ({ components, pureComponents }) => () => {
 
     return <div>
-        <div id="header">
+        <div id="contact-me-link">
             <a href="#contact-me">CONTACT ME</a>
         </div>
-        <pureComponents.TitleBar />
-        <pureComponents.Portfolio />
+        <div id="header">
+            <pureComponents.TitleBar />
+        </div>
+        <div id="content">
+            <pureComponents.Portfolio />
+        </div>
         <div id="footer">
             <components.ContactMe />
         </div>
