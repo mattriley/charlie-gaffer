fca1104da5a6ee3e4465ff01cbe5a0a5610ee057
Matt Riley <mattrileyaus@gmail.com>
2022-08-05 10:09:08 +1000

    Unspecified changes

--- src/default-config.js
+++ src/default-config.js
@@ -4,7 +4,7 @@ module.exports = {
     portfolio: require('./portfolio.json'),
     stage: process.env.STAGE,
     appName: process.env.DISPLAY_NAME,
-    contactUrl: process.env.CONTACT_URL,
+    bffUrl: process.env.BFF_URL,
     googleRecaptchaSiteKey: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
     googleAnalyticsTrackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
     contactMeInitialState: { name: '', email: '', phone: '', message: '', grecaptchaResponse: '' }
--- src/modules/effects/send-message.js
+++ src/modules/effects/send-message.js
@@ -1,6 +1,6 @@
 module.exports = ({ io, config }) => message => {
 
-    const url = `${config.contactUrl}/messages`;
+    const url = `${config.bffUrl}/messages`;
     return io.fetchJson(url, { method: 'POST', body: message });
 
 };
